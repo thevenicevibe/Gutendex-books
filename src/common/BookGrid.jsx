66de441291a30475c887a6bf0bc391979dfc49ad
Jyoti Singh <singhjyoti1409@gmail.com>
2024-09-06 14:48:55 +0530

    final commit

--- src/common/BookGrid.jsx
+++ src/common/BookGrid.jsx
@@ -1,32 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import BookCard from './BookCard';
-import "./BookGrid.css"
+    import React, { useState, useEffect } from 'react';
+    import BookCard from './BookCard';
+    import "./BookGrid.css"
 
-const BooksGrid = ({results}) => {
-  const [books, setBooks] = useState([]);
+    const BooksGrid = ({results}) => {
+    const [books, setBooks] = useState([]);
 
-  // Fetch books from API
-  useEffect(() => {
-    fetch("http://skunkworks.ignitesol.com:8000/books?search=")
-      .then(response => response.json())
-      .then(data => {
-        setBooks(data.results); // Assuming the array is in data.results
-      })
-      .catch(error => console.error("Error fetching books:", error));
-  }, []);
+    // Fetch books from API
+    useEffect(() => {
+        const fetchBooks = async () => {
+        
+        const response = await fetch(`http://skunkworks.ignitesol.com:8000/books?search=${results}`)
+        const data = await response.json()
+        if (data.results) {
+            setBooks(data.results);
+        } else {
+            setBooks([]);
+        }
+        }
+        if (results) {
+            fetchBooks();
+        } else {
+            setBooks([]); 
+        }
+    }, [results]);
 
-  return (
-    <div className="books-grid">
-      {books.map((book) => (
-        <BookCard
-          key={book.id}
-          title={book.title}
-          authors={book.authors}
-          coverImage={book.formats["image/jpeg"] || "default-cover.jpg"}
-        />
-      ))}
-    </div>
-  );
-};
+    return (
+        <div className="books-grid">
+        {books.length > 0 ? (
+            books.map((book, index) => (
+            <BookCard
+            key={book.id}
+            title={book.title}
+            authors={book.authors}
+            coverImage={book.formats["image/jpeg"] || "default-cover.jpg"}
+            />
+        ))
+        ) :(
+            <p>No books found.</p>
+        )
+        }
+        </div>
+    );
+    };
 
-export default BooksGrid;
+    export default BooksGrid;
